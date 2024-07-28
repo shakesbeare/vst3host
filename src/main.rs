0fcbb2f214129524928573b6ad2fd219560beabc
Berint Moffett <berint.moffett@gmail.com>
2024-07-28 15:19:30 -0600

    BROKEN: use com library to implement VST3 interfaces

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,28 @@ use core_foundation::base::TCFType;
 use core_foundation::bundle::CFBundle;
 use core_foundation::string::CFString;
 use core_foundation::url::{kCFURLPOSIXPathStyle, CFURL};
-use std::borrow::BorrowMut;
 use std::ffi::{CStr, CString};
 use std::mem::MaybeUninit;
 use std::str::FromStr;
 use vst3_sys::base::{
-    tresult, FIDString, IPluginBase, IPluginFactory, IUnknown, PClassInfo, PFactoryInfo,
+    kResultFalse, kResultTrue, tresult, FIDString, IPluginBase, IPluginFactory,
+    IUnknown, PClassInfo, PFactoryInfo,
 };
-use vst3_sys::c_void;
 use vst3_sys::gui::{IPlugFrame, IPlugView, ViewRect};
-use vst3_sys::sys::{GUID, HRESULT};
+use vst3_sys::sys::GUID;
 use vst3_sys::utils::SharedVstPtr;
 use vst3_sys::vst::{
     kVstAudioEffectClass, IEditController, IoModes, IID_IEDIT_CONTROLLER,
 };
 use vst3_sys::vst::{IComponent, IID_ICOMPONENT};
 use vst3_sys::VstPtr;
+use vst3_sys::{c_void, VST3};
 use winit::dpi::{LogicalSize, PhysicalSize};
 use winit::event::WindowEvent;
 use winit::event_loop::EventLoop;
+#[allow(deprecated)]
 use winit::raw_window_handle::{
-    DisplayHandle, HasDisplayHandle, HasRawWindowHandle, RawDisplayHandle,
-    RawWindowHandle,
+    HasRawWindowHandle, RawWindowHandle,
 };
 
 use libloading::{Library, Symbol};
@@ -33,11 +33,13 @@ const VST_PATH: &str = "/Library/Audio/Plug-Ins/VST3/OTT.vst3";
 #[cfg(target_os = "macos")]
 const OS_LABEL: &str = "MacOS";
 
+#[allow(dead_code)]
 trait ViewRectExt {
     fn to_logical_size(self) -> winit::dpi::LogicalSize<f32>;
     fn eq(&self, other: &ViewRect) -> bool;
 }
 
+#[allow(dead_code)]
 trait ToViewRectExt {
     fn to_view_rect(self) -> ViewRect;
 }
@@ -75,10 +77,12 @@ impl ViewRectExt for ViewRect {
     }
 
     fn eq(&self, other: &ViewRect) -> bool {
-        match (self.right == other.right, self.bottom == other.bottom, self.top == other.top, self.left == other.left) {
-            (true, true, true, true) => true,
-            _ => false,
-        }
+        matches!((
+            self.right == other.right,
+            self.bottom == other.bottom,
+            self.top == other.top,
+            self.left == other.left,
+        ), (true, true, true, true))
     }
 }
 
@@ -139,47 +143,70 @@ fn main() -> anyhow::Result<()> {
         .with_max_level(tracing::Level::WARN)
         .init();
 
-    let module = Module::new(VST_PATH);
-    if module.entry() {
-        println!("module loaded");
-    }
-    let factory = module.factory();
-    println!("module has {:?} class(es)", unsafe {
-        factory.count_classes()
-    });
+    let event_loop = EventLoop::new().unwrap();
+    // let mut view = View {
+    //     window: None,
+    //     plug_view: plugview,
+    //     reference_count: 1,
+    //     __iunknownvptr: todo!(),
+    //     __iplugframevptr: todo!(),
+    //     __refcnt: 1.into(),
+    //
+    // };
+    let mut view = View::new();
+    event_loop.run_app(&mut view)?;
 
-    let mut factory_info: MaybeUninit<PFactoryInfo> = MaybeUninit::uninit();
-    unsafe { factory.get_factory_info(factory_info.as_mut_ptr()) };
-    let mut factory_info: PFactoryInfo = unsafe { factory_info.assume_init() };
+    Ok(())
+}
 
-    let url = unsafe { CStr::from_ptr(factory_info.url.as_mut_ptr()) };
-    let email = unsafe { CStr::from_ptr(factory_info.email.as_mut_ptr()) };
-    let vendor = unsafe { CStr::from_ptr(factory_info.vendor.as_mut_ptr()) };
+#[VST3(implements(IUnknown, IPlugFrame))]
+struct View {
+    window: Option<winit::window::Window>,
+    plug_view: VstPtr<dyn IPlugView>,
+}
 
-    println!("{:?}", url);
-    println!("{:?}", email);
-    println!("{:?}", vendor);
+impl View {
+    pub(crate) fn new() -> Box<View> {
+        let module = Module::new(VST_PATH);
+        if module.entry() {
+            println!("module loaded");
+        }
+        let factory = module.factory();
+        println!("module has {:?} class(es)", unsafe {
+            factory.count_classes()
+        });
 
-    let mut class_info: MaybeUninit<PClassInfo> = MaybeUninit::uninit();
-    unsafe { factory.get_class_info(0, class_info.as_mut_ptr()) };
-    let mut class_info: PClassInfo = unsafe { class_info.assume_init() };
+        let mut factory_info: MaybeUninit<PFactoryInfo> = MaybeUninit::uninit();
+        unsafe { factory.get_factory_info(factory_info.as_mut_ptr()) };
+        let mut factory_info: PFactoryInfo = unsafe { factory_info.assume_init() };
 
-    let category = unsafe { CStr::from_ptr(class_info.category.as_mut_ptr()) };
-    let name = unsafe { CStr::from_ptr(class_info.name.as_mut_ptr()) };
+        let url = unsafe { CStr::from_ptr(factory_info.url.as_mut_ptr()) };
+        let email = unsafe { CStr::from_ptr(factory_info.email.as_mut_ptr()) };
+        let vendor = unsafe { CStr::from_ptr(factory_info.vendor.as_mut_ptr()) };
 
-    println!("{:?}", category);
-    println!("{:?}", name);
+        println!("{:?}", url);
+        println!("{:?}", email);
+        println!("{:?}", vendor);
 
-    // instantiate the classes
-    for i in 0..unsafe { factory.count_classes() } {
+        let mut class_info: MaybeUninit<PClassInfo> = MaybeUninit::uninit();
+        unsafe { factory.get_class_info(0, class_info.as_mut_ptr()) };
+        let mut class_info: PClassInfo = unsafe { class_info.assume_init() };
+
+        let category = unsafe { CStr::from_ptr(class_info.category.as_mut_ptr()) };
+        let name = unsafe { CStr::from_ptr(class_info.name.as_mut_ptr()) };
+
+        println!("{:?}", category);
+        println!("{:?}", name);
+
+        // instantiate the classes
         let mut info: MaybeUninit<PClassInfo> = MaybeUninit::uninit();
-        unsafe { factory.get_class_info(i, info.as_mut_ptr()) };
+        unsafe { factory.get_class_info(0, info.as_mut_ptr()) };
         let mut info: PClassInfo = unsafe { info.assume_init() };
 
         let category = unsafe { CStr::from_ptr(info.category.as_mut_ptr()) };
         let expected = unsafe { CStr::from_ptr(kVstAudioEffectClass) };
         if category != expected {
-            continue;
+            panic!("unexpected category on line 212");
         }
 
         let mut component: MaybeUninit<VstPtr<dyn IComponent>> = MaybeUninit::uninit();
@@ -192,7 +219,7 @@ fn main() -> anyhow::Result<()> {
         };
 
         if create_instance_result != vst3_sys::base::kResultTrue {
-            anyhow::bail!(
+            panic!(
                 "Failed to properly create IComponent instance, error code: {}",
                 create_instance_result
             );
@@ -215,7 +242,7 @@ fn main() -> anyhow::Result<()> {
         };
 
         if create_instance_result != vst3_sys::base::kResultTrue {
-            anyhow::bail!(
+            panic!(
                 "Failed to properly create IEditController instance, error code: {}",
                 create_instance_result
             );
@@ -227,87 +254,36 @@ fn main() -> anyhow::Result<()> {
         // TODO: pass the host context IHostApplication
         unsafe { edit.initialize(std::ptr::null_mut()) };
 
-        let view_ptr = unsafe { edit.create_view(CString::new("editor")?.as_ptr()) };
-        let plugview = unsafe {
+        let view_descriptor = CString::new("editor").unwrap();
+        let view_ptr = unsafe { edit.create_view(view_descriptor.as_ptr()) };
+        let plug_view = unsafe {
             std::mem::transmute::<
                 *mut vst3_sys::c_void,
                 vst3_sys::VstPtr<dyn vst3_sys::gui::IPlugView>,
             >(view_ptr)
         };
 
-        let event_loop = EventLoop::new().unwrap();
-        let mut view = View {
-            window: None,
-            plug_view: plugview,
-        };
-
-        event_loop.run_app(&mut view)?;
-    }
-
-    Ok(())
-}
-
-struct View {
-    window: Option<winit::window::Window>,
-    plug_view: VstPtr<dyn IPlugView>,
-}
-
-impl IUnknown for View {
-    #[doc = r" The COM [`QueryInterface` Method]"]
-    #[doc = r""]
-    #[doc = r" This method normally should not be called directly. Interfaces that implement"]
-    #[doc = r" `IUnknown` also implement [`IUnknown::get_interface`] which is a safe wrapper around"]
-    #[doc = r" `IUnknown::query_interface`."]
-    #[doc = r""]
-    #[doc = r" [`QueryInterface` Method]: https://docs.microsoft.com/en-us/windows/win32/api/unknwn/nf-unknwn-iunknown-queryinterface(refiid_void)"]
-    #[doc = r" [`IUnknown::get_interface`]: trait.IUnknown.html#method.get_interface"]
-    unsafe fn query_interface(
-        &self,
-        riid: *const GUID,
-        ppv: *mut *mut c_void,
-    ) -> HRESULT {
-        todo!()
-    }
-
-    #[doc = r" The COM [`AddRef` Method]"]
-    #[doc = r""]
-    #[doc = r" This method normally should not be called directly. This method is used by"]
-    #[doc = r" [`ComRc`] to implement the reference counting mechanism."]
-    #[doc = r""]
-    #[doc = r" [`AddRef` Method]: https://docs.microsoft.com/en-us/windows/win32/api/unknwn/nf-unknwn-iunknown-addref"]
-    #[doc = r" [`ComRc`]: struct.ComRc.html"]
-    unsafe fn add_ref(&self) -> u32 {
-        todo!()
-    }
-
-    #[doc = r" The COM [`Release` Method]"]
-    #[doc = r""]
-    #[doc = r" This method normally should not be called directly. This method is used by"]
-    #[doc = r" [`ComRc`] to implement the reference counting mechanism."]
-    #[doc = r""]
-    #[doc = r" [`Release` Method]: https://docs.microsoft.com/en-us/windows/win32/api/unknwn/nf-unknwn-iunknown-release"]
-    #[doc = r" [`ComRc`]: struct.ComRc.html"]
-    unsafe fn release(&self) -> u32 {
-        todo!()
+        return Self::allocate(None, plug_view);
     }
 }
 
 impl IPlugFrame for View {
     unsafe fn resize_view(
         &self,
-        view: SharedVstPtr<dyn IPlugView>,
+        _view: SharedVstPtr<dyn IPlugView>,
         new_size: *mut ViewRect,
     ) -> tresult {
         let new_size = PhysicalSize::<u32> {
             width: (*new_size).right as u32,
             height: (*new_size).bottom as u32,
         };
-        self.window.as_ref().unwrap().request_inner_size(new_size);
-        0
+        let _ = self.window.as_ref().unwrap().request_inner_size(new_size);
+        kResultTrue
     }
 }
 
 impl View {
+    #[allow(deprecated)]
     fn raw_handle(&self) -> Option<RawWindowHandle> {
         if let Some(win) = &self.window {
             let window_handle = win.raw_window_handle().unwrap();
@@ -322,7 +298,7 @@ impl View {
             RawWindowHandle::AppKit(ptr) => {
                 let ptr = ptr.ns_view.as_ptr();
                 let platform_ui =
-                    FIDString::from(CString::new("X11EmbedWindowID")?.as_ptr());
+                    FIDString::from(CString::new("NSView")?.as_ptr());
                 unsafe {
                     self.plug_view.attached(ptr, platform_ui);
                 }
@@ -359,9 +335,12 @@ impl winit::application::ApplicationHandler for View {
                 vst3_sys::gui::ViewRect,
             >(view_rect)
         };
-        let self_ = unsafe { &mut *(self as *mut Self) };
+        let self_ = unsafe { &mut *(self as *mut Self) } as *mut Self as *mut c_void;
+        if unsafe { self.plug_view.can_resize() } == kResultFalse {
+            self.window.as_ref().unwrap().set_resizable(false);
+        }
         unsafe {
-            self.plug_view.set_frame(self_ as *mut Self as *mut c_void);
+            self.plug_view.set_frame(self_);
         }
         let _ = self
             .window
@@ -395,18 +374,7 @@ impl winit::application::ApplicationHandler for View {
                 std::process::exit(0);
             }
             WindowEvent::Resized(size) => {
-                let mut desired = size
-                    .to_logical(self.window.as_ref().unwrap().scale_factor())
-                    .to_view_rect();
-                let mut actual = desired.clone();
-                let res = unsafe { self.plug_view.check_size_constraint(&mut actual as *mut ViewRect)};
-                if !desired.eq(&actual) {
-                    dbg!("not equal!");
-                    let _ = self.window.as_ref().unwrap().request_inner_size(actual.to_logical_size());
-                }
-
-                let _resize_result =
-                    unsafe { self.plug_view.on_size(&mut actual as *mut ViewRect) };
+                tracing::info!("Window resized to {:?}", size);
             }
             _ => {}
         }
